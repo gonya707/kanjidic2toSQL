8290aaf285a27bb8db24ee42235ff709419a2a8e
Gonzalo Recio <gonya707@gmail.com>
2015-03-20 17:01:15 +0100

    Parsed readings and basic data

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		parse("assets/kanjidic2.xml");
+		System.out.println("Done.");
 	}
 
 	private static void parse(String url) {
@@ -33,11 +34,86 @@ public class Main {
 		Document doc = Jsoup.parse(xml);
 		Elements characters = doc.getElementsByTag("character");
 
-		int i =1 ;
 		for (Element character : characters) {
 			//String href = character.attr("href");
+
+			// Single data tags
 			String literal = character.getElementsByTag("literal").text();
+			String grade = character.getElementsByTag("grade").text();
+			String stroke_count = character.getElementsByTag("stroke_count").text();
+			String freq = character.getElementsByTag("literal").text();
+			String jlpt = character.getElementsByTag("jlpt").text();
+
+			// readings
+			String ja_on = "";
+			String ja_kun = "";
+			String pinyin = "";
+			String korean_r = "";
+			String korean_h = "";
+
+			Elements readings = doc.getElementsByTag("reading");
+			for (Element reading : readings) {
+				String r_type = reading.attr("r_type");
+				switch(r_type){
+				case "ja_on":
+					if (!ja_on.equals("")){
+						ja_on = reading.text();
+						}
+					else{
+						ja_on = ", " + reading.text();
+					}
+					break;
+				case "ja_kun":
+					if (!ja_kun.equals("")){
+						ja_kun = reading.text();
+						}
+					else{
+						ja_kun = ", " + reading.text();
+					}
+					break;
+				case "pinyin":
+					if (!pinyin.equals("")){
+						pinyin = reading.text();
+						}
+					else{
+						pinyin = ", " + reading.text();
+					}
+					break;
+				case "korean_r":
+					if (!korean_r.equals("")){
+						korean_r = reading.text();
+						}
+					else{
+						korean_r = ", " + reading.text();
+					}
+					break;
+				case "korean_h":
+					if (!korean_h.equals("")){
+						korean_h = reading.text();
+						}
+					else{
+						korean_h = ", " + reading.text();
+					}
+					break;
+
+				}
+
+			}
 			
+			// nanori readings
+			String nanori = "";
+			
+			Elements e_nanoris = doc.getElementsByTag("nanori");
+			for (Element e_nanori : e_nanoris) {
+				if (!nanori.equals("")){
+					nanori = e_nanori.text();
+					}
+				else{
+					nanori = ", " + e_nanori.text();
+				}
+			}
+
+
 		}	
 
 
